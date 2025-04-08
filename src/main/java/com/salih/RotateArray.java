4ae6eca0f3543dd52b2105a65a95739964039f73
ahmethlos <salihasd99@gmail.com>
2025-04-08 21:50:50 +0300

    bubble sort rotate

--- src/main/java/com/salih/RotateArray.java
+++ src/main/java/com/salih/RotateArray.java
@@ -1,19 +1,18 @@
 package com.salih;
 
-import java.util.Scanner;
 
-import static java.util.Collections.reverse;
+public class RotateArray {
+    /*
+    Problem: Rotate an array of n elements to the right by k steps.
+    For example, with n= 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+    How many different ways do you know to solve this problem?
+     */
 
-public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        int n = 7;
 
-        System.out.print("Enter the number of elements in the array: ");
-        int n = scanner.nextInt();
-
-        System.out.print("Enter the number of steps to rotate: ");
-        int k = scanner.nextInt();
+        int k = 3;
 
         //generate array
         int[] nums = new int[n];
@@ -38,6 +37,14 @@ public class Main {
         endTime = System.nanoTime();
         System.out.println("Rotated using reversal algorithm: " + java.util.Arrays.toString(nums2));
         System.out.println("Time taken (reversal algorithm): " + (endTime - startTime) + " ns");
+
+        // Measure time for rotateUsingBubbleSort
+        int[] nums3 = nums.clone();
+        startTime = System.nanoTime();
+        rotateUsingBubbleSort(nums3, k);
+        endTime = System.nanoTime();
+        System.out.println("Rotated using bubble sort: " + java.util.Arrays.toString(nums3));
+        System.out.println("Time taken (bubble sort): " + (endTime - startTime) + " ns");
     }
 
     public static void rotateUsingExtraArray(int[] nums, int k) {
@@ -47,7 +54,7 @@ public class Main {
         for(int i=0; i < k; i++){
             result[i] = nums[nums.length-k+i];
         }
-        System.out.println("result: " + java.util.Arrays.toString(result));
+        //result: [5, 6, 7, 0, 0, 0, 0]
         int j=0;
         for(int i=k; i<nums.length; i++){
             result[i] = nums[j];
@@ -57,7 +64,6 @@ public class Main {
     }
 
     public static void rotateUsingReversal(int[] nums, int k) {
-
         k=k%nums.length;
         reverse(nums, 0, nums.length-1);
         reverse(nums, 0, k-1);
@@ -74,7 +80,15 @@ public class Main {
         }
     }
 
-
-
+    public static void rotateUsingBubbleSort(int[] nums, int k) {
+        k=k%nums.length;
+        for (int i = 0; i < k; i++) {
+            for (int j = nums.length - 1; j > 0; j--) {
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+            }
+        }
+    }
 
 }
